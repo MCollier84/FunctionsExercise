860d9ff43f94931cb6544fb26914d489ab7447cf
Maurice Collier <mauricecollier84@yahoo.com>
2021-10-20 00:01:46 -0400

    Created the FunctionsExercise file.

---
+++ script.js
@@ -0,0 +1,90 @@
+// 1
+function spookyGhost(){
+    console.log(`Boo!!!`);
+}
+
+spookyGhost();
+
+console.log(`=======`);
+
+// 2
+function candy(goal){
+    console.log(`${goal} ${goal} ${goal} ${goal}`); 
+}
+
+candy(`Get Candy`);
+
+console.log(`=======`);
+
+// 3
+function wereWolf(a, b){
+    if (true, true)
+    console.log(`${a} ${b}`);
+}
+wereWolf(`Full`, `Moon`);
+
+console.log(`=======`);
+
+// 4
+function halloween(){
+    return `October 31st`;
+}
+
+console.log(halloween());
+
+console.log(`=======`);
+
+// 5
+function isEnoughCandy(pieces){
+    if (pieces >= `31`){
+        return true;
+    }
+    return 
+}
+
+console.log(`=========`)
+
+// 6
+function candyCounter(num){
+    let total = 0;
+    for (n of num) {
+        total = total + n;
+    }
+    return total;
+}
+console.log(candyCounter([11, 23, 35, 43]));
+console.log(candyCounter([99, 199]));
+
+console.log("=======");
+
+// 7
+const hauntedMansion = function (){
+    console.log("Welcome, foolish mortals, to the Haunted Mansion!");
+}
+hauntedMansion();
+
+console.log("=======");
+
+// 8
+ghostbusters = () => "Who you gonna call?"
+console.log(ghostbusters());
+
+console.log("=======");
+
+// 9
+// no
+
+// 10
+// Yes
+
+// 11a
+const costumes = ["Ghost", "Princess", "Pirate"];
+
+// 11b
+let costumeFunction = function (){
+    const upperCostumes = costumes.map(name => name.toUpperCase());
+    console.log(upperCostumes);
+}
+costumeFunction();
+
+console.log(`==========`);
